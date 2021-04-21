dc8978a3b8372c4a090e117bad09fca53c351a9d
Nico Apablaza <73095954+nicosuke@users.noreply.github.com>
2021-04-22 08:30:26 +1200

    Update pet.js

--- yo/pet.js
+++ yo/pet.js
@@ -10,11 +10,35 @@ class Pet {
  }   
 }
 //aki vamos a meter a una mascota nueva
-
+//en vez de tipear retoa la wea pa crear un objeto, tipeas esto y se crea.
 const tommy = new Pet ('dog', 5, 'stray', 'wuf');
-//en vez de tipear retoa la wea pa crear un objeto.
+const picho = new Pet ('parrot', 6, 'galah', 'picho');
+const alex = new Pet ('parrot', 'unknown', 'alexandrine', 'pukekohe');
+const pipi = new Pet ('bird', '4 months', 'sparrow', 'mi');
 
 tommy.speak();
+alex.speak();
+picho.speak();
+
+//getter method
+get activity() {
+const today = new Date();
+const hour = today.getHours();
+
+if(hour > 8 && hour <= 20) {
+    return 'playing';
+} else { 
+    return 'sleeping';
+}
+}
+
+console.log(pipi.activity);
+//no me sirvio, pero sirve pa que cuando accedes
+//a un objeto puedes ver q hacer ese objeto
+// a que hora x ejemplo, y te da un value
+//sleeping, pero ese sleeping no esta
+//guardado en el objeto en si, hmmmmm
+
 
 /*
 class Student {
